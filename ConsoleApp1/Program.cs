a700c175af8a7db413d7a52bf6e4ddef70fc574a
muathmm <127548222+muathmm@users.noreply.github.com>
2024-06-03 11:10:13 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { Console.WriteLine("Hello, Worl1!");
             Console.WriteLine("Hello, World!");
         }
     }
